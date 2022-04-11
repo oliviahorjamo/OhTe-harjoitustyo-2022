4088c9cbf1a4a0b6344a9f980b662db62b41f678
Olivia Horjamo <olivia.horjamo@gmail.com>
2022-04-11 16:06:47 +0300

    Create tests for adding and deleting numbers and separate sudoku -classes

--- src/tests/sudoku_service_test.py
+++ src/tests/sudoku_service_test.py
@@ -1,29 +1,100 @@
-#sisältää sudokun pelilogiikan testit
-#3.4.2022 pelilogiikkaa ei vielä lainkaan SudokuService -luokassa vaan pelkästään index.py tiedostossa
+# sisältää sudokun pelilogiikan testit
+from entities.sudoku import Sudoku, OriginalSudoku
+import unittest
 import sys
 import os
+from services.sudoku_service import sudoku_service
 
 #sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 print(sys.path)
 
-import unittest
-from entities.sudoku import Sudoku
 
 class TestSudoku(unittest.TestCase):
     def setUp(self):
-        #sudoku on nyt moduuli
+        # sudoku on nyt moduuli
         print("ollaan setupissa")
+        self.originals = OriginalSudoku(1)
         self.sudoku = Sudoku(1)
+
+    def test_create_original_sudoku(self):
+        self.assertEqual(str(self.originals.grid), str([
+            [7, 8, 0, 4, 0, 0, 1, 2, 0],
+            [6, 0, 0, 0, 7, 5, 0, 0, 9],
+            [0, 0, 0, 6, 0, 1, 0, 7, 8],
+            [0, 0, 7, 0, 4, 0, 2, 6, 0],
+            [0, 0, 1, 0, 5, 0, 9, 3, 0],
+            [9, 0, 4, 0, 6, 0, 0, 0, 5],
+            [0, 7, 0, 3, 0, 0, 0, 1, 2],
+            [1, 2, 0, 0, 0, 7, 4, 0, 0],
+            [0, 4, 9, 2, 0, 6, 0, 0, 7]
+        ]))
 
     def test_create_sudoku(self):
         self.assertEqual(str(self.sudoku.grid), str([
-        [7, 8, 0, 4, 0, 0, 1, 2, 0],
-        [6, 0, 0, 0, 7, 5, 0, 0, 9],
-        [0, 0, 0, 6, 0, 1, 0, 7, 8],
-        [0, 0, 7, 0, 4, 0, 2, 6, 0],
-        [0, 0, 1, 0, 5, 0, 9, 3, 0],
-        [9, 0, 4, 0, 6, 0, 0, 0, 5],
-        [0, 7, 0, 3, 0, 0, 0, 1, 2],
-        [1, 2, 0, 0, 0, 7, 4, 0, 0],
-        [0, 4, 9, 2, 0, 6, 0, 0, 7]
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]))
+
+    def test_add_number(self):
+        self.assertEqual(sudoku_service.add_number(self.originals.grid, self.sudoku, 2, 2, 2), True)
+        self.assertEqual(str(self.sudoku.grid), str([
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 2, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]))
+
+    def test_add_number_not_allowed(self):
+        self.assertEqual(sudoku_service.add_number(self.originals.grid, self.sudoku, 0, 0, 2), False)
+        self.assertEqual(str(self.sudoku.grid), str([
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]))
+
+    def test_delete_number(self):
+        sudoku_service.add_number(self.originals.grid, self.sudoku, 0, 0, 2)
+        self.assertEqual(sudoku_service.delete_number(self.originals.grid, self.sudoku, 2, 2), True)
+        self.assertEqual(str(self.sudoku.grid), str([
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]))
+
+    def test_delete_number_not_allowed(self):
+        self.assertEqual(sudoku_service.delete_number(self.originals.grid, self.sudoku, 0, 0), False)
+        self.assertEqual(str(self.originals.grid), str([
+            [7, 8, 0, 4, 0, 0, 1, 2, 0],
+            [6, 0, 0, 0, 7, 5, 0, 0, 9],
+            [0, 0, 0, 6, 0, 1, 0, 7, 8],
+            [0, 0, 7, 0, 4, 0, 2, 6, 0],
+            [0, 0, 1, 0, 5, 0, 9, 3, 0],
+            [9, 0, 4, 0, 6, 0, 0, 0, 5],
+            [0, 7, 0, 3, 0, 0, 0, 1, 2],
+            [1, 2, 0, 0, 0, 7, 4, 0, 0],
+            [0, 4, 9, 2, 0, 6, 0, 0, 7]
         ]))
